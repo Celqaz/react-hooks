122cdf747294d9053b13db0dacebdbe22864824f
“celqaz” <819248392@qq.com>
2021-03-27 22:50:01 +0800

    useEffect

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,23 +4,45 @@
 import * as React from 'react'
 
 // Cool Custom Hook
-function useSyncState(key, defaultValue='') {
+function useSyncState(
+  key,
+  defaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
   // ||或运算，如果左侧为False，则返回右侧数据。
   // lazy state initialization
-  const [state, setState] = React.useState(
-    () => window.localStorage.getItem(key) || defaultValue
-  )
-  
+  const [state, setState] = React.useState(() => {
+    const valueInstorage = window.localStorage.getItem(key)
+    // console.log(key, valueInstorage)
+    if (valueInstorage) {
+      // JSON.parse()方法将数据转换为JavaScript对象。
+      // valueInstorage此时为字符串，
+      // const att = JSON.parse(valueInstorage)
+      // return valueInstorage
+      return deserialize(valueInstorage)
+    }
+    // give the feature to create the function that create the defaultValue dynamic
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const preKeyRef = React.useRef(key)
+
   React.useEffect(() => {
-    // console.log(state)
-    window.localStorage.setItem(key, state)
-  },[key, state]) //Effect Dependency
-  
-  return [state,setState]
+    const prevKey = preKeyRef.current
+    if (prevKey != key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    preKeyRef.current = key
+    // JSON.stringify() 方法将 JavaScript 对象转换为字符串
+    window.localStorage.setItem(key, serialize(state))
+    // window.localStorage.setItem(key, state)
+  }, [key, serialize, state]) //Effect Dependency
+
+  return [state, setState]
 }
 
 function Greeting({initialName = ''}) {
-  const [name, setName] =  useSyncState('name',initialName)
+  const [name, setName] = useSyncState('name', initialName)
   console.log(name)
   function handleChange(event) {
     setName(event.target.value)
